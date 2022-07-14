eba7253b1457dfeaf352a4e65ff3c78ec77e4d21
Patrick Boivin <pboivin@gmail.com>
2022-07-14 11:34:44 -0400

    Alternative - Use paginated query

--- app/Http/Livewire/PostList.php
+++ app/Http/Livewire/PostList.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
@@ -10,14 +11,14 @@ class PostList extends Component
     const ITEMS_PER_PAGE = 12;
 
     public $postIdChunks = [];
-    public $page = 1;
+    public $page = 0;
     public $maxPage = 1;
     public $sortDirection = 'asc';
     public $queryCount = 0;
 
     public function mount()
     {
-        $this->prepareChunks();
+        $this->loadMore();
     }
 
     public function render()
@@ -27,33 +28,35 @@ class PostList extends Component
 
     public function updatedSortDirection()
     {
-        $this->prepareChunks();
+        $this->resetList();
     }
 
     public function loadMore()
     {
         if ($this->hasNextPage()) {
             $this->page++;
+
+            /** @var LengthAwarePaginator */
+            $paginator = DB::table('posts')
+                ->orderBy('title', $this->sortDirection)
+                ->paginate(self::ITEMS_PER_PAGE, ['id'], 'page', $this->page);
+
+            $this->postIdChunks[] = $paginator->pluck('id');
+
+            $this->maxPage = $paginator->lastPage();
         }
-    }
-
-    public function prepareChunks()
-    {
-        $this->postIdChunks = DB::table('posts')
-            ->orderBy('title', $this->sortDirection)
-            ->pluck('id')
-            ->chunk(self::ITEMS_PER_PAGE)
-            ->toArray();
-
-        $this->page = 1;
-
-        $this->maxPage = count($this->postIdChunks);
-
-        $this->queryCount++;
     }
 
     public function hasNextPage()
     {
         return $this->page < $this->maxPage;
+    }
+
+    public function resetList()
+    {
+        $this->page = 0;
+        $this->postIdChunks = [];
+        $this->queryCount++;
+        $this->loadMore();
     }
 }
